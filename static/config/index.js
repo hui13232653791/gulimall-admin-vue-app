a9d98a873348bb2908a35eff539017fbd5b56ace
wangyh <1942590224@qq.com>
2022-06-25 12:49:41 +0800

    谷粒商城管理后台前端

--- static/config/index.js
+++ static/config/index.js
@@ -7,6 +7,9 @@
   // api接口请求地址
   window.SITE_CONFIG['baseUrl'] = 'http://localhost:88/api';
 
+  //k8s线上环境
+  window.SITE_CONFIG['baseUrl'] = 'http://192.168.196.138:30007/api';
+
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
   window.SITE_CONFIG['version'] = '';   // 版本号(年月日时分)
